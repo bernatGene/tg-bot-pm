5ee8c5635a26d7b728bee7285eb612474e1b714d
Bernat Skrabec <bernat.skrabec@marss.com>
2022-12-11 18:00:33 +0100

    finsih updade using gsdb

--- streamlitapp.py
+++ streamlitapp.py
@@ -15,7 +15,6 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
 
-
 st.title("Telegram bot dashboard")
 
 HELP = "commands /start, /register, /yesterday <hh> <mm>"
@@ -23,21 +22,23 @@ HELP = "commands /start, /register, /yesterday <hh> <mm>"
 # @st.experimental_singleton
 def _get_db():
     scopes = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
     ]
     secrets = dict(st.secrets)
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(secrets, scopes)
-    file = gspread.authorize(credentials) # authenticate the JSON key with gspread
-    sheet = file.open("TheTimeSink") #open sheet
+    file = gspread.authorize(credentials)  # authenticate the JSON key with gspread
+    sheet = file.open("TheTimeSink")  # open sheet
     return sheet.worksheet("database")
 
 
-def _get_dataframe():
+def _get_dataframe(return_db=False):
     db = _get_db()
     df = pd.DataFrame(db.get_all_records()).set_index("Dia")
-    df.index = pd.to_datetime(df.index)
+    df.index = pd.to_datetime(df.index, dayfirst=True)
     df = df.applymap(lambda x: pd.to_timedelta(x, errors="coerce"))
+    if return_db:
+        return df, db
     return df
 
 
@@ -48,7 +49,9 @@ def read_column(column):
 
 def _username(update):
     user = update.message.from_user
-    username = user.username or user.first_name or user.name or user.full_name or user.id
+    username = (
+        user.username or user.first_name or user.name or user.full_name or user.id
+    )
     username = username.lower()
     return username
 
@@ -67,88 +70,103 @@ def register_user(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=resp)
 
 
-def summary(update: Update, context: CallbackContext):
+def _check_user(update, context, df = None):
     username = _username(update)
-    df = _get_dataframe()
+    if df is None:
+        df = _get_dataframe()
     users = df.columns
     if username not in users:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Usuari no registrat")
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Usuari no registrat"
+        )
         return
-    
+    return username
+
+
+def summary(update: Update, context: CallbackContext):
+    df = _get_dataframe()
+    username = _check_user(update, context, df=df)
     val = df[username]
-    total, high, low, avg = val.sum().components, val.max().components, val.min().components, val.mean().components
+    total, high, low, avg = (
+        val.sum().components,
+        val.max().components,
+        val.min().components,
+        val.mean().components,
+    )
     t = f"En total has passat {total.days} dies, {total.hours} hores i {total.minutes} minuts mirant el mobil."
     h = f"El teu record en un dia es de {high.hours} hores i {high.minutes} minuts."
     l = f"I el minim que has aconseguit es de {low.hours} hores i {low.minutes} minuts."
     a = f"La teva mitjana diaria es de {avg.hours} hores i {avg.minutes} minuts."
-    for resp in [t,h,l,a]:
+    for resp in [t, h, l, a]:
         context.bot.send_message(chat_id=update.effective_chat.id, text=resp)
-    
+
 
 def start(update: Update, context: CallbackContext):
     user = update.message.from_user
-    username = user.username or user.first_name or user.name or user.full_name or user.id
+    username = (
+        user.username or user.first_name or user.name or user.full_name or user.id
+    )
     username = username.lower()
     resp = f"Fa pal escriure descripcio, hola {username}, i tal. Commandes son: " + HELP
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=resp
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=resp)
 
 
 def reminder(update: Update, context: CallbackContext):
-    users = Path("./").glob("*.csv")
+    df = _get_dataframe()
+    _yesterday = pd.Timestamp(date.today() - pd.Timedelta(days=1))
+    users = df.columns
+    was_empty = False
+    if _yesterday not in df.index:
+        df.loc[_yesterday] = [pd.NaT]*len(users)
+        was_empty = True
+    values = df.loc[_yesterday]
     done = []
     pending = []
-    for user_db_path in users:
-        db_user = pd.read_csv(user_db_path, index_col="date", parse_dates=[0], infer_datetime_format=True) 
-        username = '@' + user_db_path.stem
-        if pd.Timestamp(date.today() - pd.Timedelta(days=1)) in db_user.index:
-            done.append(username)
+    for usr, val in zip(users, values):
+        if pd.isnull(val):
+            pending.append('@' + usr)
         else:
-            pending.append(username)
+            done.append('@' + usr)
+
     if not done:
         message = "Ningu ha marcat encara el seu us d'ahir"
     elif not pending:
         message = "Tots els usuaris han marcat l'us d'ahir"
     elif len(pending) == 1:
-        message = f"Tots els usuaris han marcat l'us d'ahir, excepte {pending[0]}, espavila."
+        message = (
+            f"Tots els usuaris han marcat l'us d'ahir, excepte {pending[0]}, espavila."
+        )
     else:
         message = f"{', '.join(done)} ja han marcat, falten {', '.join(pending)}."
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    
+
 
 def yesterday(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    username = user.username or user.first_name or user.name or user.full_name or user.id
-    username = username.lower()
     value = context.args
-    user_db = Path(f"{username}.csv")
-    if not user_db.is_file():
-        resp = "User not registered. Write /register to do so."
     if len(value) != 2 or not all([v.isdigit() and len(v) == 2 for v in value]):
         resp = "Commanda en mal format. Escriu: '/yesterday hh mm' (hours minutes)"
-    else:
-        try:
-            db_user = pd.read_csv(user_db, index_col="date", parse_dates=[0], infer_datetime_format=True) 
-            delta  = pd.Timedelta(hours=int(value[0]), minutes=int(value[1]))
-            db_user.loc[pd.Timestamp(date.today() - pd.Timedelta(days=1))] = delta
-            db_user.to_csv(user_db)
-            resp = f"M'apunto que ahir vas passar {value[0]} hores i {value[1]} minuts."
-        except ValueError:
-            resp = f"Something went wrong. try again later (I'm still building this!)"
-    
+        context.bot.send_message(chat_id=update.effective_chat.id, text=resp)
+        return
+    df, db = _get_dataframe(return_db=True)
+    username = _check_user(update, context, df=df)
+    # delta = pd.Timedelta(hours=int(value[0]), minutes=int(value[1])).seconds
+    delta_str = f"{int(value[0]):02d}:{int(value[1]):02d}:00"
+    _yesterday = pd.Timestamp(date.today() - pd.Timedelta(days=1))
+    row = df.index.get_loc(_yesterday) + 2
+    col = df.columns.get_loc(username) + 2
+    db.update_cell(row, col, delta_str)
+    resp = f"M'apunto que ahir vas passar {value[0]} hores i {value[1]} minuts."
     context.bot.send_message(chat_id=update.effective_chat.id, text=resp)
     reminder(update, context)
 
+
 def _create_updater():
     token = st.secrets["BOT_TOKEN"]
-    updater = Updater(
-        token=token, use_context=True
+    updater = Updater(token=token, use_context=True)
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
     )
-    # logging.basicConfig(
-    #     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    #     level=logging.INFO,
-    # )
     dispatcher = updater.dispatcher
     start_handler = CommandHandler("start", start)
     dispatcher.add_handler(start_handler)
@@ -194,16 +212,19 @@ def stop_bot():
         return
     st.text("It is not running")
 
+
 def restart_bot():
     updater = get_updater()
     if updater["active"]:
         stop_bot()
     start_telegram_bot()
 
+
 def show_st_dataframe():
     df = _get_dataframe()
     st.code(df)
     st.write(df.dtypes)
+
 
 st.button("Start bot", on_click=start_telegram_bot)
 st.button("Stop bot", on_click=stop_bot)
